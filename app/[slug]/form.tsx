f545b193af613ad5c3355668754975bd51171493
febxx <41862424+febxx@users.noreply.github.com>
2024-07-28 20:16:46 +0700

    init

---
+++ app/[slug]/form.tsx
@@ -0,0 +1,119 @@
+import { useTwibbonCanvas } from "@/hooks/useTwibbonCanvas";
+import { useEffect, useState } from "react";
+import Canvas from "./canvas";
+import { FaFileImage } from "react-icons/fa";
+import { FaCopy, FaDownload } from "react-icons/fa6";
+
+interface Props {
+  data: {
+    title: string;
+    slug: string;
+    creator: string;
+    image: string;
+  }
+}
+
+function downloadURI(uri: string, name: string) {
+  const link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+export default function Form({ data }: Readonly<Props>) {
+  const { image, title, slug } = data;
+  const canvasHook = useTwibbonCanvas();
+
+  const [fileName, setFileName] = useState<string>();
+  const [scale, setScale] = useState<number>(1);
+
+  useEffect(() => {
+    if (slug) {
+      window.history.pushState({}, "", `/${slug}`);
+    }
+
+    canvasHook.addBackground(image);
+  }, [image, slug, canvasHook]);
+
+  useEffect(() => {
+    canvasHook.setScaled(scale);
+  }, [scale, canvasHook]);
+
+  return (
+    <div className="flex flex-col gap-4 p-4 rounded-md">
+      <div className="flex justify-center items-center space-y-4 flex-col">
+        <Canvas
+          width={canvasHook.recommendedSize.width}
+          height={canvasHook.recommendedSize.height}
+          canvasid="twibbon"
+          ref={canvasHook.canvasRef}
+        />
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <button
+          className="rounded-full bg-primary py-2 px-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+          onClick={() => setScale((prev) => prev - 0.01)}
+        >
+          -
+        </button>
+        <input
+          id="zoom"
+          type="range"
+          min="0.2"
+          max="3"
+          step="0.01"
+          value={scale}
+          onChange={(e) => {
+            setScale(parseFloat(e.currentTarget.value));
+          }}
+          className="bg-primary hover:bg-primary/80 !w-[10rem] md:!w-[15rem] mx-2"
+        />
+        <button
+          className="rounded-full bg-primary py-2 px-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+          onClick={() => setScale((prev) => prev + 0.01)}
+        >
+          +
+        </button>
+      </div>
+      <div className="flex flex-row items-center justify-around">
+        <input
+          type="file"
+          id="foto"
+          accept="image/png, image/jpeg, image/jpg"
+          onChange={async (ev) => {
+            setFileName(ev.currentTarget.files?.[0]?.name);
+
+            if (ev.currentTarget.files?.length) {
+              canvasHook.addFrame(
+                URL.createObjectURL(ev.currentTarget.files[0])
+              );
+            }
+          }}
+          hidden
+        />
+        <label
+          htmlFor="foto"
+          className="flex items-center gap-2 truncate max-w-[18rem] md:max-w-sm rounded-md bg-black/20 py-3 px-4 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30 cursor-pointer duration-100"
+        >
+          <FaFileImage /> Choose image
+        </label>
+        <button
+          onClick={() => {
+            const data = canvasHook.toDataUrl();
+            if (data) {
+              downloadURI(
+                data,
+                `Twibbon ${title}.jpg`
+              );
+            }
+          }}
+          className="rounded-md flex items-center gap-2 bg-primary py-3 px-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+        >
+          <FaDownload /> Download
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ app/[slug]/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import Form from "./form";
+import { useTwibbonCanvas } from "@/hooks/useTwibbonCanvas";
+
+const SlugPage = () => {
+  const { slug } = useParams();
+  const router = useRouter();
+  const canvasHook = useTwibbonCanvas();
+
+  const items = [
+    {
+      title: "Kata X: 2nd Tourney",
+      slug: "katax-2nd-tourney",
+      creator: "@febxx",
+      image: "/images/template/Kata X 2nd Tourney.png",
+    },
+  ];
+
+  const item = items.find((item) => item.slug === slug);
+
+  useEffect(() => {
+    if (!item) {
+      router.replace("/error");
+    }
+  }, [item, router]);
+
+  if (!item) {
+    return (
+      <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
+        <div className="container">
+          <div className="flex items-center justify-center">
+            <svg aria-hidden="true" role="status" className="inline w-10 h-10 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
+            </svg>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                {item.title}
+              </h3>
+              <p className="mb-11 text-center text-base font-medium text-body-color">
+                {item.creator}
+              </p>
+              <div className="mx-auto max-w-[550px] rounded-md bg-primary bg-opacity-5 dark:bg-dark pt-3"> {/* py-10 px-6 sm:p-[60px] */}
+                <Form data={item}></Form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-0 left-0 z-[-1]">
+          <svg
+            width="1440"
+            height="969"
+            viewBox="0 0 1440 969"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask
+              id="mask0_95:1005"
+              style={{ maskType: "alpha" }}
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="1440"
+              height="969"
+            >
+              <rect width="1440" height="969" fill="#090E34" />
+            </mask>
+            <g mask="url(#mask0_95:1005)">
+              <path
+                opacity="0.1"
+                d="M1086.96 297.978L632.959 554.978L935.625 535.926L1086.96 297.978Z"
+                fill="url(#paint0_linear_95:1005)"
+              />
+              <path
+                opacity="0.1"
+                d="M1324.5 755.5L1450 687V886.5L1324.5 967.5L-10 288L1324.5 755.5Z"
+                fill="url(#paint1_linear_95:1005)"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="paint0_linear_95:1005"
+                x1="1178.4"
+                y1="151.853"
+                x2="780.959"
+                y2="453.581"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint1_linear_95:1005"
+                x1="160.5"
+                y1="220"
+                x2="1099.45"
+                y2="1192.04"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SlugPage;
