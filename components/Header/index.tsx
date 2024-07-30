e125d94357e8a5e63c36c48ef459de0325be6383
febxx <41862424+febxx@users.noreply.github.com>
2024-07-30 17:05:12 +0700

    feat: update header menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import ThemeToggler from "./ThemeToggler";
 import menuData from "./menuData";
+import { FaRightToBracket } from "react-icons/fa6";
 
 const Header = () => {
   // Navbar toggle
@@ -139,15 +140,9 @@ const Header = () => {
               <div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
                   href="/signin"
-                  className="hidden py-3 px-7 text-base font-bold text-dark hover:opacity-70 dark:text-white md:block"
+                  className="ease-in-up rounded-md py-3 px-8 text-base font-bold text-xl text-dark dark:text-white transition duration-300 md:block md:px-9 lg:px-6 xl:px-9"
                 >
-                  Sign In
-                </Link>
-                <Link
-                  href="/signup"
-                  className="ease-in-up hidden rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9"
-                >
-                  Sign Up
+                  <FaRightToBracket />
                 </Link>
                 <div>
                   <ThemeToggler />
--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -9,22 +9,28 @@ const menuData: Menu[] = [
   },
   {
     id: 2,
-    title: "About",
-    path: "/about",
+    title: "Explore",
+    path: "/explore",
     newTab: false,
   },
+  // {
+  //   id: 3,
+  //   title: "About",
+  //   path: "/about",
+  //   newTab: false,
+  // },
   // {
   //   id: 33,
   //   title: "Blog",
   //   path: "/blog",
   //   newTab: false,
   // },
-  {
-    id: 3,
-    title: "Support",
-    path: "/contact",
-    newTab: false,
-  },
+  // {
+  //   id: 4,
+  //   title: "Support",
+  //   path: "/contact",
+  //   newTab: false,
+  // },
   // {
   //   id: 4,
   //   title: "Pages",
